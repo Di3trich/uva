06f2e34d8bf6be53044cfcf0def3cbdf2ea624a7
Jonathan Alberth Quispe Fuentes <qf.jonathan@gmail.com>
2016-11-03 05:30:58 -0500

    espiral tap

--- P2-10920.cpp
+++ P2-10920.cpp
@@ -2,20 +2,31 @@
 using namespace std;
 
 int main(){
-    int sz, p;
-
-    while(cin >> sz >> p && sz!=0 && p!=0){
-        int l = 1, up = 0;
-        while( p > l*4-4 ){
-            p -= l*4-4;
-            l+=2;
-        }
-        l-=3;
-        int f = sz/2+l+1, c = sz/2+1;
-        while( p-- ){
-            
-        }
+    
+    int l;
+    long long p;
+    
+    while( cin >> l >> p, l && p ){
+        long long ld = ceill(sqrtl(p));
+        if( ld%2 == 0 ) ld++;
+        p -= (ld-2) * (ld-2);
+        
+        int f = (l+1)/2 + ld/2, c = (l+1)/2 + ld/2;
+        
+        if( p <= ld-1 ) c -= p, p = 0;
+        else c -= ld-1, p -= ld-1;
+        
+        if( p <= ld-1 ) f -= p, p = 0;
+        else f -= ld-1, p -= ld-1;
+        
+        if( p <= ld-1 ) c += p, p = 0;
+        else c += ld-1, p -= ld-1;
+        
+        if( p <= ld-1 ) f += p, p = 0;
+        else f += ld-1, p -= ld-1;
+        
+        cout << "Line = " << f << ", column = " << c << "." << endl;
     }
-
+    
     return 0;
 }
